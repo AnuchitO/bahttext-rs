ca3e808bbb478f1c2ea74dd7d306f81884d27612
AnuchitO <3372362+AnuchitO@users.noreply.github.com>
2025-09-07 15:06:52 +0700

    single digit 0 to 3

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,10 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
 pub fn words(money: f64) -> String {
-    return "ศูนย์บาทถ้วน".to_string()
+    match money {
+        3.0 => "สามบาทถ้วน".to_string(),
+        2.0 => "สองบาทถ้วน".to_string(),
+        1.0 => "หนึ่งบาทถ้วน".to_string(),
+        _ => "ศูนย์บาทถ้วน".to_string(),
+    }
 }
 
 #[cfg(test)]
@@ -21,7 +22,20 @@ mod tests {
     fn words_single_digit_one() {
         let got = words(1.0);
 
-        assert_eq!(got, "ศูนย์บาทถ้วน");
+        assert_eq!(got, "หนึ่งบาทถ้วน");
     }
 
+    #[test]
+    fn words_single_digit_two() {
+        let got = words(2.0);
+
+        assert_eq!(got, "สองบาทถ้วน");
+    }
+
+    #[test]
+    fn words_single_digit_three() {
+        let got = words(3.0);
+
+        assert_eq!(got, "สามบาทถ้วน");
+    }
 }
