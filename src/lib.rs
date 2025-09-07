29d42b84af0056958ba2c8d16b70892941a294c5
AnuchitO <3372362+AnuchitO@users.noreply.github.com>
2025-09-07 22:10:23 +0700

    use pattern matching

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,34 @@
 pub fn words(money: f64) -> String {
-    match money {
-        3.0 => "สามบาทถ้วน".to_string(),
-        2.0 => "สองบาทถ้วน".to_string(),
-        1.0 => "หนึ่งบาทถ้วน".to_string(),
-        _ => "ศูนย์บาทถ้วน".to_string(),
+    let unit_words = [
+        "",
+        "หนึ่ง",
+        "สอง",
+        "สาม",
+        "สี่",
+        "ห้า",
+        "หก",
+        "เจ็ด",
+        "แปด",
+        "เก้า",
+    ];
+    /*
+           preciseAmount := math.Round(math.Abs(money)*100) / 100
+        wholeBaht := math.Trunc(preciseAmount)
+        satang := math.Round((preciseAmount - wholeBaht) * 100)
+
+        bahtText := moneyToThaiWords(uint64(wholeBaht))
+
+        if satang == 0 {
+            return minus + bahtText + "บาทถ้วน"
+        }
+    */
+    let precise_amount = (money.abs() * 100.0) / 100.0;
+    let whole_baht = precise_amount.trunc() as u64;
+    let satang = ((precise_amount - whole_baht as f64) * 100.0).round() as u64;
+
+    match (whole_baht, satang) {
+        (0, _) => "ศูนย์บาทถ้วน".to_string(),
+        (_, _) => format!("{}บาทถ้วน", unit_words[whole_baht as usize]),
     }
 }
 
@@ -18,6 +43,12 @@ mod tests {
             ("one", 1.0, "หนึ่งบาทถ้วน"),
             ("two", 2.0, "สองบาทถ้วน"),
             ("three", 3.0, "สามบาทถ้วน"),
+            ("four", 4.0, "สี่บาทถ้วน"),
+            ("five", 5.0, "ห้าบาทถ้วน"),
+            ("six", 6.0, "หกบาทถ้วน"),
+            ("seven", 7.0, "เจ็ดบาทถ้วน"),
+            ("eight", 8.0, "แปดบาทถ้วน"),
+            ("nine", 9.0, "เก้าบาทถ้วน"),
         ];
 
         for &(name, money, expect) in &cases {
